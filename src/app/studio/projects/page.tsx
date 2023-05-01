5463b059fd917fa2b112784ee0c6b9e071c24087
kelvinpraises <kelvinpraises@gmail.com>
2023-05-01 20:49:18 +0100

    feat: restructure studio

---
+++ src/app/studio/projects/page.tsx
@@ -0,0 +1,27 @@
+import Card from "@/components/Card";
+import Tag from "@/components/Tag";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const page = () => {
+  return (
+    <main className="flex flex-col">
+      <div className=" px-8 pb-8 flex w-full flex-col gap-8">
+        <div className=" w-full flex gap-4 ">
+          <input className=" bg-[#181A1F] border-[#303236] border-[1px] w-full rounded-lg focus:outline-none py-4 px-6" />
+          <button className=" px-5 py-4 bg-white whitespace-nowrap text-[#050505] grid place-items-center rounded-lg">
+            <p className=" font-semibold">Add New Project</p>
+          </button>
+        </div>
+        <div className=" grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 gap-4 w-full ">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default page;
