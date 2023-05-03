28b4415df0fa903366b73d24c304f801e2d453e5
kelvinpraises <kelvinpraises@gmail.com>
2023-05-03 20:55:23 +0100

    fix: add funtion body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HeaderL from "@/components/HeaderL";
 import Link from "next/link";
 import React from "react";
 
+const Page = () => {
   return (
     <div className=" ">
       <HeaderL />
@@ -60,4 +61,4 @@ import React from "react";
   );
 };
 
-export default page;
+export default Page;
