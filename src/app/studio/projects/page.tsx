b3e1687574d49c3d6cd6270d83133efbd605feab
kelvinpraises <kelvinpraises@gmail.com>
2023-05-03 21:07:59 +0100

    fix: lint warnings

--- src/app/studio/projects/page.tsx
+++ src/app/studio/projects/page.tsx
@@ -29,7 +29,7 @@ const Page = () => {
         </div>
         <div className=" grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 gap-4 w-full ">
           {communities.map((data, i) => (
-            <Card />
+            <Card key={i} />
           ))}
         </div>
       </div>
